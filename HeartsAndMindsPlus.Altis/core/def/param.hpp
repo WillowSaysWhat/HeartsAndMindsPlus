afd676ea9e0a474f5347786838ac484e6ca42e1a
Tetlys <joshrobertshaw@me.com>
2024-09-03 11:21:14 +0100

    Turned animals off

--- HeartsAndMindsPlus.Altis/core/def/param.hpp
+++ HeartsAndMindsPlus.Altis/core/def/param.hpp
@@ -213,7 +213,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_SPWAN_ANIMALS_DENSITY"]);
         values[]={0,10,20,30,40,50,60,70,80,90,100};
         texts[]={"0%","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
-        default = 10;
+        default = 0;
     };
     class btc_p_veh_armed_ho { // Add armed vehicles in Hideout:
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_SPAWN_ARMEDHIDEOUT"]);
